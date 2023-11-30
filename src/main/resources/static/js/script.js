cdc074e30e10fd55f88bf32c726bf3ab93c32c25
Stéfany Marques <stefany.marques06@gmail.com>
2023-11-30 16:55:45 -0300

    Ajustes Front-End da área Staff

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -7,3 +7,16 @@ function cpfMask() {
         cpfInput.value += '-'
     }
 }
+
+function updateTime() {
+    const timeElement = document.getElementById('time');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    timeElement.textContent = timeString;
+}
+
+setInterval(updateTime, 1000); // Atualiza a cada segundo
+updateTime();
